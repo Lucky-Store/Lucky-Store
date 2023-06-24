4af9ba5e112c1fa8ad53d8540e769e3ae57d5700
Lucky-Store <137507148+Lucky-Store@users.noreply.github.com>
2023-06-24 03:22:56 +0300

    Update useSignup.jsx

--- frontend/src/hooks/useSignup.jsx
+++ frontend/src/hooks/useSignup.jsx
@@ -12,7 +12,7 @@ export const useSignup = () => {
     setIsLoading(true);
     setError(null);
 
-    const res = await fetch("http://localhost:3000/api/v1/user/signup", {
+    const res = await fetch("https://lucky-store.onrender.com/api/v1/user/signup", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
