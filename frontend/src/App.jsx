e31d4b7933984b9b0b3183e07fb2d0e4191adcbf
Lucky-Store <137507148+Lucky-Store@users.noreply.github.com>
2023-06-24 19:43:15 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -34,7 +34,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/store" element={<Store />} />
-        <Route path="/store/:id" element={<Product />} />
+        <Route path="/product/:id" element={<Product />} />
         <Route
           path="/*"
           element={
